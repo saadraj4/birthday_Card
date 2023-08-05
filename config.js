30fcc8c5dedf3b50a1a323a3e4466f7c6d97a93a
saadraj4 <sr201040@gmail.com>
2023-08-06 03:16:44 +0500

    Files

---
+++ config.js
@@ -0,0 +1,33 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "On this special day, I’m wishing you a special celebration",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "Eat sweet cake",  // 同上...
+        "pop up your favorite drink",
+        "and enjoy with your friends and family",
+        "May this special day be just the beginning of a year filled with",
+        "unforgettable moments, laughter, and endless happiness",
+        "I hope you will receive the love and care you deserve!🎂🥳",
+        " I hope your day is filled with everything you love and that you feel truly celebrated.",
+        "May ALLAH bless you and keep you safe always",
+        "Once again,",
+        "Happy Birthday",
+    ],
+
+    imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai.png",
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "Click",
+        play: "Some Music?",
+        bannar_coming: "Lights?",
+        balloons_flying: "It seems that something is missing",
+        light_candle: "Candle?",
+        wish_message: "Happy birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
